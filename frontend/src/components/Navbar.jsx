a70198a99fa5adca9c6b55b3a714e454cf0bf873
Zeyad <zeyad.officemail@gmail.com>
2025-06-02 09:11:14 +0300

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -108,7 +108,7 @@ const Navbar = ({ onSelectCategory, onSearch }) => {
         <nav className="navbar navbar-expand-lg fixed-top">
           <div className="container-fluid">
             <a className="navbar-brand" href="https://www.linkedin.com/in/zeyad-ahmed-513871322/">
-              GearBox
+              Flipzy
             </a>
             <button
               className="navbar-toggler"
